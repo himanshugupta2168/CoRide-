6ebb45d0b5f11294f105773b3efff64df32426c9
himanshu2168.be21@chitkara.edu.in <himanshu2168.be21@chitkara.edu.in>
2024-04-24 15:19:32 +0530

    branch for backend routes

--- Frontend/src/components/SearchRide.jsx
+++ Frontend/src/components/SearchRide.jsx
@@ -34,11 +34,12 @@ function SearchRide() {
     startPoint: '',
     endPoint: '',
     date: new Date(),
-    passengers: '1',
+    passengers: 1,
   });
+  const [updatePassenger, setUpdatePassenger]= useState(false);
 
   return (
-    <div className='w-full bg-green-400 h-[150px] flex items-center text-white'>
+    <div className='hidden md:flex w-full bg-green-400 h-[200px]  items-center text-white relative'>
       <div className='flex w-[90vw] px-4 mx-auto border border-green-600 h-[60px] m-4 items-center rounded-lg gap-4 '>
         <SearchInput start={true} setRide={setRide} ride={ride} />
         <SearchInput start={false} setRide={setRide} ride={ride} />
@@ -51,11 +52,22 @@ function SearchRide() {
           />
           <div className='w-1 h-full bg-slate-400 rounded-lg'></div>
         </div>
-        <div className='w-[25%] h-full flex items-center gap-2 py-2'>
+        <div className='w-[25%] h-full flex items-center gap-2 py-2' onClick={(e)=>{setUpdatePassenger(!updatePassenger)}}>
           <IoPersonOutline />
-          <p className='font-semibold'>{ride.passengers} passengers</p>
+          <p className='font-semibold' >{ride.passengers} passengers</p>
         </div>
+        <div className=' px-6 py-2 bg-black rounded-lg '>Search</div>
       </div>
+      {updatePassenger&& 
+          <div className=' absolute right-16 w-[25%] h-[51px] bottom-2  border border-green-600 rounded-md flex px-2 items-center justify-between'>
+          <div className='text-lg font-semibold'>Passenger</div>
+          <div className="vary flex gap-4 px-4">
+            <span className=' w-8 h-8 text-center font-bold  rounded-full text-xl cursor-pointer' onClick={(e)=>{setRide({...ride,passengers:ride.passengers-1})}} > - </span>
+            <p>{ride.passengers}</p>
+            <span className='w-8 h-8 text-center font-bold  rounded-full text-xl cursor-default' onClick={(e)=>{setRide({...ride, passengers:ride.passengers+1})}}> + </span>
+          </div>
+        </div>
+      }
     </div>
   );
 }
