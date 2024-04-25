f1fec6b1c16f9c2155a5409d355decc3bfa6dcf8
himanshu2168.be21@chitkara.edu.in <himanshu2168.be21@chitkara.edu.in>
2024-04-25 11:33:42 +0530

    added env example

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,10 +1,23 @@
-import express, { Request, Response, Errback } from "express";
+import express, { Request, Response, Errback, NextFunction } from "express";
 import { config } from "dotenv";
-
+import cors from "cors"
+import {responseCodes}  from "./config/responseCodes";
+import rideRoutes from "./routes/rideRoutes"
 const app = express();
+app.use(cors({
+    origin:"*", 
+    credentials:true, 
+}))
+app.use("/api/v1/rides",rideRoutes )
 
-
-
+app.get("/", (req, res)=>{res.send("hello")})
+app.use((err:Errback, req:Request, res:Response, next:NextFunction)=>{
+    console.error(err);
+    return res.status(responseCodes.internalServerError).json({
+        success:false, 
+        error: err
+    })
+})
 app.listen(process.env.PORT || 8000).on("error", (e: Errback) => {
     console.log("Error in listening to port", e);
 });
